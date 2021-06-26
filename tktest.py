8ae7b84c859121d5ebffa4e31db4ba8eb0a4bc41
Seb <sebastian+gh@meijome.net>
2021-06-26 12:22:21 +1000

    tktest: Fixed window size, so that the user cannot fullscreen and resize

--- tktest.py
+++ tktest.py
@@ -7,10 +7,11 @@ root.title("Python GUI Application ")
 ttk.Label(root, text="Separating widget").pack()
 #Create Panedwindow
 panedwindow=ttk.Panedwindow(root, orient=VERTICAL)
+root.resizable(width=False ,height=False)
 panedwindow.pack(fill=BOTH, expand=True)
 #Create Frams
-fram1=ttk.Frame(panedwindow,width=400,height=300, relief=SUNKEN)
-fram2=ttk.Frame(panedwindow,width=400,height=100, relief=SUNKEN)
+fram1=ttk.Frame(panedwindow,width=1000,height=600, relief=SUNKEN)
+fram2=ttk.Frame(panedwindow,width=1000,height=350, relief=SUNKEN)
 panedwindow.add(fram1, weight=1)
 panedwindow.add(fram2, weight=4)
 #Calling Main()
