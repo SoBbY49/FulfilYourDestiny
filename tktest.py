8281673d5c48e984109528522202c99183c828b8
Seb <sebastian+gh@meijome.net>
2021-06-26 12:29:53 +1000

    Added menu widget that allows user to exit

--- tktest.py
+++ tktest.py
@@ -1,4 +1,5 @@
 #Create app using with separating widget
+import tkinter
 from tkinter import *
 from tkinter import ttk
 root=Tk()
@@ -14,5 +15,13 @@ fram1=ttk.Frame(panedwindow,width=1000,height=600, relief=SUNKEN)
 fram2=ttk.Frame(panedwindow,width=1000,height=350, relief=SUNKEN)
 panedwindow.add(fram1, weight=1)
 panedwindow.add(fram2, weight=4)
+#Menu
+def quit ():
+    root.destroy ()
+bar = tkinter.Menu(root)
+fileMenu = tkinter.Menu(bar ,tearoff =0)
+fileMenu.add_command(label="Exit",command=quit)
+bar.add_cascade(label="File",menu=fileMenu)
+root.config(menu=bar)
 #Calling Main()
 root.mainloop()
