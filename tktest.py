1753e982ce7c57fc1c38d77d91d1cbb0cd2949e6
Seb <sebastian+gh@meijome.net>
2021-06-27 14:59:28 +1000

    Completely fullscreen while still being able to see game in ALT+TAB

--- tktest.py
+++ tktest.py
@@ -10,6 +10,8 @@ root = Tk()
 root.title("Fulfil Your Destiny")
 root.configure(bg="black")
 root.resizable(width=False, height=False)
+root.attributes('-fullscreen', True)
+
 # Custom menu
 bar = tkinter.Menu(root)
 fileMenu = tkinter.Menu(bar, tearoff=0)
@@ -19,8 +21,6 @@ root.config(menu=bar)
 # Fullscreen
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
-# ttk.Label(root, text="Fulfil Your Destiny", font=("Courier", 30), background='black', foreground='red').pack()
-
 # Create Panedwindow
 # FIXME need to make frames a fixed size
 panedwindow_style = ttk.Style()
@@ -29,18 +29,17 @@ panedwindow = ttk.Panedwindow(root, orient=HORIZONTAL, style="BW.TPanedwindow")
 panedwindow.pack(fill=BOTH, expand=True)
 
 # Create text areas
+art_font = Font(family="Courier", size=50)
+story_font = Font(family="Courier", size=20)
 
-art_font = Font(family="Courier",size=50)
-story_font = Font(family="Courier",size=20)
-
-txt_art = tkinter.Text(panedwindow,state = "normal", foreground="white", background="black")
-txt_story = tkinter.Text(panedwindow,state = "normal", foreground="white", background="black")
+txt_art = tkinter.Text(panedwindow, state="normal", foreground="white", background="black", width=100, height=20)
+txt_story = tkinter.Text(panedwindow, state="normal", foreground="white", background="black", width=100, height=20)
 panedwindow.add(txt_art, weight=1)
-panedwindow.add(txt_story, weight=4)
+panedwindow.add(txt_story, weight=1)
 
-txt_art.insert(END,fyd_logo())
-txt_story.insert(END,"this is the story of the 3 piggies")
-txt_story.insert("1.0","FYD starts here...")
+txt_art.insert(END, fyd_logo())
+txt_story.insert(END, "this is the story of the 3 piggies")
+txt_story.insert("1.0", "FYD starts here...")
 
 # disabling to stop user editing
 txt_art.configure(state="disabled")
