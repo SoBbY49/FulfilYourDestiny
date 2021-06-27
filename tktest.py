3162802da0203fade04ab03161a67536883c7d3f
Seb <sebastian+gh@meijome.net>
2021-06-27 11:53:05 +1000

    Removed fullscreen, now able to see game  in ALT+TAB and in task bar

--- tktest.py
+++ tktest.py
@@ -3,11 +3,20 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 
+# root. window
 root = Tk()
-
-# App Title
-root.title("Python GUI Application ")
+root.title("Fulfil Your Destiny")
 root.configure(bg="black")
+root.resizable(width=False, height=False)
+# Custom menu
+bar = tkinter.Menu(root)
+fileMenu = tkinter.Menu(bar, tearoff=0)
+fileMenu.add_command(label="Exit", command=quit)
+bar.add_cascade(label="File", menu=fileMenu)
+root.config(menu=bar)
+# FullscreenS
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+
 # ttk.Label(root, text="Fulfil Your Destiny", font=("Courier", 30), background='black', foreground='red').pack()
 
 # Create Panedwindow
@@ -15,24 +24,17 @@ root.configure(bg="black")
 panedwindow_style = ttk.Style()
 panedwindow_style.configure("BW.TPanedwindow", foreground="red", background="black", showhandle=False, sashwidth=0)
 panedwindow = ttk.Panedwindow(root, orient=HORIZONTAL, style="BW.TPanedwindow")
-root.resizable(width=False, height=False)
 panedwindow.pack(fill=BOTH, expand=True)
-
-# Fullscreen
-# FIXME you can't see this in alt tab or task bar...
-root.overrideredirect(True)
-root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
 # Create Frams
 frame_style = ttk.Style()
 frame_style.configure("BW.TFrame", foreground="black", background="black")
 fram1 = ttk.Frame(panedwindow, width=1000, height=600, style="BW.TFrame")
 fram2 = ttk.Frame(panedwindow, width=1000, height=350, style="BW.TFrame")
-
 panedwindow.add(fram1, weight=1)
 panedwindow.add(fram2, weight=4)
 
-# Text
+# Textbox
 sample_text = tkinter.Text(root, height=10)
 sample_text.pack()
 
@@ -40,12 +42,6 @@ sample_text.pack()
 def quit():
     root.destroy()
 
-
-bar = tkinter.Menu(root)
-fileMenu = tkinter.Menu(bar, tearoff=0)
-fileMenu.add_command(label="Exit", command=quit)
-bar.add_cascade(label="File", menu=fileMenu)
-root.config(menu=bar)
 
 # Calling Main()
 root.mainloop()
