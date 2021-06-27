015b52eda4fe4d1057d25c4a52c63220548a86c5
Seb <sebastian+gh@meijome.net>
2021-06-27 12:37:10 +1000

    using text areas instead of frames

--- tktest.py
+++ tktest.py
@@ -2,6 +2,7 @@
 import tkinter
 from tkinter import *
 from tkinter import ttk
+from tkinter.font import Font
 
 # root. window
 root = Tk()
@@ -14,7 +15,7 @@ fileMenu = tkinter.Menu(bar, tearoff=0)
 fileMenu.add_command(label="Exit", command=quit)
 bar.add_cascade(label="File", menu=fileMenu)
 root.config(menu=bar)
-# FullscreenS
+# Fullscreen
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
 # ttk.Label(root, text="Fulfil Your Destiny", font=("Courier", 30), background='black', foreground='red').pack()
@@ -26,22 +27,32 @@ panedwindow_style.configure("BW.TPanedwindow", foreground="red", background="bla
 panedwindow = ttk.Panedwindow(root, orient=HORIZONTAL, style="BW.TPanedwindow")
 panedwindow.pack(fill=BOTH, expand=True)
 
-# Create Frams
-frame_style = ttk.Style()
-frame_style.configure("BW.TFrame", foreground="black", background="black")
-fram1 = ttk.Frame(panedwindow, width=1000, height=600, style="BW.TFrame")
-fram2 = ttk.Frame(panedwindow, width=1000, height=350, style="BW.TFrame")
-panedwindow.add(fram1, weight=1)
-panedwindow.add(fram2, weight=4)
+# Create text areas
 
+art_font = Font(family="Courier",size=50)
+story_font = Font(family="Courier",size=20)
+
+txt_art = tkinter.Text(panedwindow,state = "normal", foreground="white", background="black")
+txt_story = tkinter.Text(panedwindow,state = "normal", foreground="white", background="black")
+panedwindow.add(txt_art, weight=1)
+panedwindow.add(txt_story, weight=4)
+
+txt_art.insert(END," art here")
+txt_story.insert(END,"this is the story of the 3 piggies")
+txt_story.insert("1.0","FYD starts here...")
+
+# disabling to stop user editing
+txt_art.configure(state="disabled")
+txt_story.configure(state="disabled")
 # Textbox
 sample_text = tkinter.Text(root, height=10)
 sample_text.pack()
 
-# Menu
+
+# Quit
 def quit():
     root.destroy()
 
 
-# Calling Main()
+# NOTHING WILL HAPPEN AFTER CALLING mainloop() UNTIL THE WINDOW IS CLOSED
 root.mainloop()
