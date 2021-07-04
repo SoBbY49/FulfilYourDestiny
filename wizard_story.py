07cc22143c68646a6912a8e5fac4f4c7d1fd9277
Seb <sebastian+gh@meijome.net>
2021-07-04 19:19:30 +1000

    updated

---
+++ wizard_story.py
@@ -0,0 +1,129 @@
+def part_3():
+    s = """
+    You wake up to find yourself in darkness.
+    You try to feel around but all you can feel is the cold ground and hear some water dripping.
+    As your eyes start to adjust to the darkness you look around once more and come face to face with a skeleton.
+    You jump back and see that it is wearing shackles, ‘How did I get here?’.
+    You get up and as you try to find a way out you accidentally stub your toe on a large stick, ow.
+    You pick it up and you see that it has a large gem on the top of it, you start handling it to see how it feels.
+    It seemed to you to be a magic staff, how you knew that even you don’t know.
+    You look back at the skeleton and get some shivers down your spine.
+    You squint your eyes and see some writing scratched into the wall next to it.
+    You walk close to it and read it out loud.
+    “Say... fireball?” FOOOOSH, there's a sudden burst of light as a large ball of fire emerges on the gem from the top of the staff you are holding.
+    You thrust the staff towards the only door and only way out you can find and the entrance explodes open.
+    You peek your head out and see bookshelf after bookshelf in the room before you.
+    You head to the books and start reading them, many not making sense to you at first you start to understand the cryptic language in which these books are written in.
+    You read a few pages and realise that it's a spell book, the page you’re reading is titled ‘Invisibility’.
+    As you continue to read the page you hear multiple pairs of footsteps coming towards you from a set of stairs.
+        What do you do?
+        a) You quickly run to the stairs, ready to bonk anyone on the head with your staff.
+        b) Cast fireball.
+        c) Cast invisibility,
+    """
+    return s
+
+
+def part_3a():
+    s = """
+    You hide behind a bookshelf that is right next to the staircase and hold the staff above your head.
+    Two peasants walk down the stairs behind a very large shrouded man with a large axe in both hands.
+    You hear one of the peasants whisper to the other peasant, `Oh no, the wizard has escaped.’.
+    You sneak up behind them and whack one of the peasants over the head.
+    He falls to the ground and the other peasant sees you and points at you with a shaky hand.
+    It seems like he is trying to talk but no words are coming out of his mouth.
+    You hit him across the head with your staff and he falls over.
+    As you look towards the large man with the large axe you see that he is face to face with you.
+        What do you do?
+        a) Whack him across the face with your staff.
+        b) Cast fireball.
+        c) Cast invisibility.
+    """
+    return s
+
+
+def part_3aa():
+    s = """
+    You swing your staff against the man's head.
+    The amount of force you put into the swing shatters the staff, the wood explodes.
+    The large gem that was on top of the staff falls the the ground.
+    The man doesn't even flinch or look affected by the blow.
+    The man picks the gem up and crushes it in his hand, he opens his hand and pours the gem that is now sand onto the ground.
+    YOU DIED
+    """
+    return s
+
+
+def part_3ab():
+    s = """
+    You say fireball and once again a ball of fire emerges from the gem on your staff.
+    You thrust it 
+    """
+    return s
+
+
+def part_3ac():
+    s = """
+    """
+    return s
+
+
+def part_3b():
+    s = """
+    You hide behind one of the bookshelves far away from the stairs.
+    You peek from behind the bookshelf and see two peasants behind a very large shrouded man with a large axe in his hands.
+    You jump into view and thrust your magic staff towards them, the large ball of fire whizzes just past the large man but explodes right next to the two peasants.
+    An explosion erupts and the peasants are incinerated, the blast from the fireball had caught the large man's hood and cloak on fire.
+    He chucks the cloak off of him and you can now see his inhumanly large muscles, he begins to charge at you.
+        What do you do?
+        a) Jump out of his way
+        b) Cast invisibility.
+    """
+    return s
+
+
+def part_3ba():
+    s = """
+    """
+    return s
+
+
+def part_3bb():
+    s = """
+    """
+    return s
+
+
+def part_3c():
+    s = """
+    Not needing to hide, you stand out in the open and see two peasants and a very large shrouded man holding a huge axe come down the stairs.
+    They walk right past you towards the small dungeon cell you were locked in.
+    The large man looks at the exploded door and sees that you’re not inside the cell.
+    The peasants start getting worried and start shaking, the large man mutters to the peasants ‘Find the wizard’.
+    The peasants nod their heads worryingly and hurry up the stairs.
+    The large man sits down on a chair in the room and starts to sharpen his large axe.
+    You just remembered that there was nothing in the spell book which told you how long the invisible spell would last.
+        What do you do?
+        a) Hide behind the man.
+        b) Keep standing there, you can probably stay invisible for a long time...right?
+        c) Go upstairs.
+    """
+    return s
+
+
+def part_3ca():
+    s = """
+    """
+    return s
+
+
+def part_3cb():
+    s = """
+    """
+    return s
+
+
+def part_3cc():
+    s = """
+    """
+    return s
